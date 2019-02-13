b64260e2016d6c5b90128b7779bdeabfa8330af2
luisxmetalx <junitho_93@hotmail.com>
2019-02-13 12:35:15 -0500

    despliegue en heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn juanApp.wsgi --log-file -
--- juanApp/settings.py
+++ juanApp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_2gcukhrf6x4*2*u4rfm)z9z2gbz23mg##$zx_rb2f+xt$oc5n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_nego.apps.AppNegoConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
